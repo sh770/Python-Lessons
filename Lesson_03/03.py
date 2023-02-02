7ffb8dad2d5d167107da20b70d17929b983dc24e
sh <sh770h@gmail.com>
2023-02-02 21:29:36 +0200

    02/02/23

---
+++ Lesson_03/03.py
@@ -0,0 +1,116 @@
+#What is the output of the following code:
+
+
+print(len("Hello, World!"))
+
+# Output: 13
+
+#What is the output of the following code:
+
+
+print("Hello, World!"[0:5])
+
+# Output: Hello
+
+#What is the output of the following code:
+
+
+text = "Hello, World!"
+print(text.replace("Hello", "Hi"))
+
+# Output: Hi, World
+
+#    What is the output of the following code:
+
+text = "Hello, World!"
+print(text.isdigit())
+
+# Output: False
+
+#What is the output of the following code:
+
+
+numbers = [1, 2, 3, 4, 5]
+print(sum(numbers))
+
+# Output: 15
+
+#What is the output of the following code:
+
+
+
+numbers = [1, 2, 3, 4, 5]
+print(min(numbers))
+
+# Output: 1
+
+#What is the output of the following code:
+
+
+numbers = [1, 2, 3, 4, 5]
+print(max(numbers))
+
+# Output: 5
+
+#What is the output of the following code:
+
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[::-1])
+
+# Output: [5, 4, 3, 2, 1]
+
+#What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers[0:3])
+
+# Output: [1, 2, 3]
+
+#What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+numbers.append(6)
+print(numbers)
+
+# Output: [1, 2, 3, 4, 5, 6]
+
+# What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 0)
+print(numbers)
+
+# Output: [0, 1, 2, 3, 4, 5]
+
+#    What is the output of the following code:
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers.remove(3)
+print(numbers)
+
+# Output: [1, 2, 4, 5]
+
+#    What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+numbers.pop()
+print(numbers)
+
+#Output: [1, 2, 3, 4]
+
+#    What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+numbers.clear()
+print(numbers)
+
+#Output: []
+
+#    What is the output of the following code:
+
+numbers = [1, 2, 3, 4, 5]
+print(len(numbers))
+
+#Output: 5
---
+++ Lesson_03/test02.py
@@ -0,0 +1,33 @@
+#     Write a program to convert the first letter of a string to uppercase. X
+
+v = "aaa"
+print(v.title())
+
+#     Write a program to count the number of occurrences of a substring in a string. X
+
+s = "aba aba aba"
+print(s.count("aba"))
+
+#     Write a program to check if a string starts with a specific substring. X
+
+
+
+#     Write a program to check if a string ends with a specific substring. X
+
+#     Write a program to find the index of a substring in a string. V
+
+#     Write a program to split a string into a list based on a specific character. V
+
+#     Write a program to replace a specific substring in a string. V
+
+#     Write a program to check if a string is alphanumeric. V
+
+#     Write a program to check if a string is alphabetic. V
+
+#     Write a program to check if a string is numeric. V
+
+#     Write a program to check if a string is lowercase. V
+
+#     Write a program to check if a string is uppercase. V
+
+#     Write a program to strip whitespace from the beginning and end of a string. X
