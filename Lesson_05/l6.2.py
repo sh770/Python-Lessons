21d1a76e577228f121fc440bec1a7531e30c15f4
sh <sh770h@gmail.com>
2023-02-09 15:44:00 +0200

    09/02/23

---
+++ Lesson_05/l6.2.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+def random_until_limit():
+    while True:
+        try:
+            limit = int(input("Enter the upper limit of the numbers: "))
+            break
+        except ValueError:
+            print("Invalid input, please enter a valid integer.")
+    
+    numbers = []
+    count = [0] * limit
+    start_time = time.time()
+    while len(numbers) < limit:
+        num = random.randint(1, limit)
+        if num not in numbers:
+            numbers.append(num)
+        count[num - 1] += 1
+        print(f'Generated number: {num}')
+    
+    end_time = time.time()
+    print('\nFrequency of each number:')
+    for i in range(1, limit + 1):
+        print(f'{i}: {count[i - 1]}')
+    print(f'\nTotal time: {end_time - start_time:.2f} seconds')
+    input("Press Enter to exit")
+
+random_until_limit()
