460779a225d29f0d8d28a6ee5abe1d14e8aef13d
sh <sh770h@gmail.com>
2023-02-16 21:43:57 +0200

    16

---
+++ Lesson_07/02.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self , model , color , cost):
+        self.model = model
+        self.color = color
+        self.cost = cost
+
+    def my(self):
+        print("hello ", self.model)
+
+p1 = Car("subaro","black",50000)            
+        
+print(list(p1.self))
+
+p1.my()
